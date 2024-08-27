5fee32c775fd5fd793a3267f6f0db3e870ab7397
Anderseon200 <anderson.barreto01@uptc.edu.co>
2024-08-26 22:20:53 -0500

    icons_list

--- app/src/main/java/com/example/twitchclone/MainActivity.kt
+++ app/src/main/java/com/example/twitchclone/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.twitchclone
 import android.os.Bundle
 import android.widget.GridLayout
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -38,9 +39,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fillPostList() {
-        val imageResId = R.drawable.special_event
-        for (i in 0 until 40) {
-            postList.add(Transmission(imageResId,"Username $i"))
+        val titles = listOf(
+            "Ninja", "Ibai", "auronplay", "Rubius", "KaiCenat", "xQc", "TheGrefg", "Tfue",
+            "juansguarnizo", "Shroud", "ElMariana", "ElSpreen", "Pokimane", "Sodapoppin",
+            "Clix", "Heelmike", "Tommyinnit", "Myth", "AdinRoss", "Alanzoka", "SypherPK",
+            "TimTheTatman", "AriGameplays", "Mongraal", "Riot Games", "NICKMERCS", "Quackity",
+            "ESLCS", "summit1g", "Amouranth", "Dream", "rivers_gg", "Jynxzi", "Fortnite",
+            "Robleis", "elded", "loud_coringa", "Bugha", "Moistcr1tikal", "NickEh30",
+            "MontanaBlack88", "loltyler1"
+        )
+        val imageResIds = listOf(
+            R.drawable.ninja, R.drawable.ibai, R.drawable.auronplay, R.drawable.rubius,
+            R.drawable.kaicenat, R.drawable.xqc, R.drawable.thegrefg, R.drawable.tfue,
+            R.drawable.juansguarnizo, R.drawable.shroud, R.drawable.elmariana, R.drawable.elspreen,
+            R.drawable.pokimane, R.drawable.sodapoppin, R.drawable.clix, R.drawable.heelmike,
+            R.drawable.tommyinnit, R.drawable.myth, R.drawable.adinross, R.drawable.alanzoka,
+            R.drawable.sypherpk, R.drawable.timthetatman, R.drawable.arigameplays, R.drawable.mongraal,
+            R.drawable.riotgames, R.drawable.nickmercs, R.drawable.quackity, R.drawable.eslcs,
+            R.drawable.summit1g, R.drawable.amouranth, R.drawable.dream, R.drawable.rivers_gg,
+            R.drawable.jynxzi, R.drawable.fortnite, R.drawable.robleis, R.drawable.elded,
+            R.drawable.loud_coringa, R.drawable.bugha, R.drawable.moistcr1tikal, R.drawable.nickeh30,
+            R.drawable.montanablack88, R.drawable.loltyler1
+        )
+        for (i in titles.indices) {
+            postList.add(Transmission(imageResIds[i], titles[i]))
         }
     }
 
@@ -50,6 +72,9 @@ class MainActivity : AppCompatActivity() {
 
             val tvUsername: TextView = postView.findViewById(R.id.textView)
             tvUsername.text = post.title
+
+            val imageView: ImageView = postView.findViewById(R.id.imageView)
+            imageView.setImageResource(post.imageResId)
 
             val params = GridLayout.LayoutParams().apply {
                 width = GridLayout.LayoutParams.WRAP_CONTENT
