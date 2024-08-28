dece6d8ca7c6fae57ef03918d68dce80d93edcec
Anderseon200 <anderson.barreto01@uptc.edu.co>
2024-08-27 23:21:57 -0500

    toaster

--- app/src/main/java/com/example/twitchclone/rv_activity/adapters/RVAdapterPosts.kt
+++ app/src/main/java/com/example/twitchclone/rv_activity/adapters/RVAdapterPosts.kt
@@ -3,11 +3,13 @@ package com.example.twitchclone.rv_activity.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.twitchclone.R
 import com.example.twitchclone.data.Transmission
+import com.example.twitchclone.utils.showToast
 
 class RVAdapterPosts(private val posts: List<Transmission>) :
     RecyclerView.Adapter<RVAdapterPosts.PostViewHolder>() {
@@ -15,6 +17,7 @@ class RVAdapterPosts(private val posts: List<Transmission>) :
     class PostViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvUserName: TextView = view.findViewById(R.id.textView)
         val imageView: ImageView = view.findViewById(R.id.imageView)
+        val imageButtonChannel: ImageButton = view.findViewById(R.id.imageButtonChannel)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
@@ -26,6 +29,9 @@ class RVAdapterPosts(private val posts: List<Transmission>) :
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         holder.tvUserName.text = posts[position].title
         holder.imageView.setImageResource(posts[position].imageResId)
+        holder.imageButtonChannel.setOnClickListener {
+            it.context.showToast("Channel button clicked for ${posts[position].title}")
+        }
     }
 
     override fun getItemCount(): Int = posts.size
